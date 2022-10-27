c056304de77c0c39e81cd5d91188343fd3d84a87
pompomway <a0931086279@gmail.com>
2022-10-27 22:04:15 +0900

    SessionID

---
+++ src/main/java/com/an/TheServletSessionIdApplication.java
@@ -0,0 +1,13 @@
+package com.an;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheServletSessionIdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheServletSessionIdApplication.class, args);
+	}
+
+}
